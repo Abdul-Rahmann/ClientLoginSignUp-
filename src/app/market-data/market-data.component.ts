825ebd7a8bd4c4de299ec2554b2e3e6b05056f38
Abdul-Rahman <abdulrahmanadetsi@gmail.com>
2021-12-06 22:18:37 -0800

    Portfolio forms added

--- src/app/market-data/market-data.component.ts
+++ src/app/market-data/market-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { MarketData } from '../model/market-data.model';
+import { HttpClient,HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-market-data',
@@ -7,19 +8,26 @@ import { MarketData } from '../model/market-data.model';
   styleUrls: ['./market-data.component.css']
 })
 export class MarketDataComponent implements OnInit {
+  private exchange1url = "https://exchange.matraining.com/md";
+  private exchange2url = "https://exchange2.matraining.com/md";
 
-  @Input()
-  marketDataExchange1?:any;
-  @Input()
-  marketDataExchange2?:any;
+  exchange1: any;
+  exchange2: any;
 
-  @Input()
-  ttl?:string;
+  // @Input()
+  // marketDataExchange1?:any;
+  // @Input()
+  // marketDataExchange2?:any;
+
+  // @Input()
+  // ttl?:string;
   
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get(this.exchange1url).subscribe(md => this.exchange1= md);
+    this.http.get(this.exchange2url).subscribe(md => this.exchange2= md);
   }
 
 }
